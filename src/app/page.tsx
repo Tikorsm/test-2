9163c253fc9a087c8db535b19d6f828b262abd34
Tikorsm <isaiassaomiguel8@gmail.com>
2023-10-20 14:14:25 -0300

    commit4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import { Header } from '@/components/Header/Header'
 import { Main } from '@/components/Main/Main'
 import { Aside } from '@/components/Aside/Aside'
-
+import { Footer } from '@/components/Footer/Footer'
 
 export default function Home() {
   return (
@@ -10,7 +10,7 @@ export default function Home() {
       <Header/>
       <Main/>
       <Aside/>
-       
+      <Footer/> 
     </section>
   )
 }
